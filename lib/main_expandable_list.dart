fbe331daed00325ce37fabbc4d2a9e0b4599cc1d
Archanciel <jp.schnyder@gmail.com>
2023-03-03 19:41:41 +0100

    cosmetic

--- lib/main_expandable_list.dart
+++ lib/main_expandable_list.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         title: 'MVVM Example',
         home: Scaffold(
           appBar: AppBar(
-            title: Text('MVVM Example'),
+            title: const Text('MVVM Example'),
           ),
           body: ListViewWidget(),
         ),
